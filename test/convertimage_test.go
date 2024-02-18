67e828455383aaaa1f33559a821b91a666334cb5
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-02-18 14:25:19 +0000

    refactor(test): move test to the separate folder

--- test/convertimage_test.go
+++ test/convertimage_test.go
@@ -1,15 +1,16 @@
-package convertimage
+package test
 
 import (
 	"fmt"
+	"job-recorder-go/internal/convertimage"
 	"path/filepath"
 	"testing"
 )
 
 func TestImg2word(t *testing.T) {
-	uploadsPath := filepath.Join("..", "uploads", "test_image.png")
-	ocrPath := filepath.Join("..", "uploads", "test_image.txt")
-	word, err := Img2word(&uploadsPath, &ocrPath)
+	uploadsPath := filepath.Join("..", "assets", "uploads", "test_image.png")
+	ocrPath := filepath.Join("..", "assets", "uploads", "test_image.txt")
+	word, err := convertimage.Img2word(&uploadsPath, &ocrPath)
 	if err != nil {
 		t.Errorf("error when calling img2word: %s", err)
 	}
---
+++ test/main_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestConvertimage(t *testing.T) {
+	uploadDir := "testdata"
+
+	// Implement the clipboard reading and image saving logic here
+	imgData, _ := os.ReadFile("2024-02-18-14-15-10.951.png")
+	// Assume imgData is PNG encoded. Save it to the upload directory.
+	uploadFileTime := time.Now().Format("2006-01-02-15-04-05.000")
+	uploadFileName := uploadFileTime + ".png"
+	filePath := filepath.Join(uploadDir, uploadFileName) // Consider generating unique names
+	err := os.WriteFile(filePath, imgData, 0644)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+}
